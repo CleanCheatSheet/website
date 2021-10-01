fa460b3d1f553d7fdca4791110e50e5ea7b24130
Lea Juskiewenski <lea.jski@gmail.com>
2021-10-01 10:38:21 +0200

    responsive design

--- components/tabs.js
+++ components/tabs.js
@@ -6,17 +6,23 @@ export function Tabs(props) {
     return (
         <div className={styles.tabs}>
       
+        {/* <div className= {styles.insidetabs}> */}
         <input type="radio" name="radio2" value="3" id="tab-1" />
-        <label for="tab-1" className= {styles.tabsone} >
+        <label for="tab-1" className= {styles.insidetabs}>
             <p>Search through the basics</p></label>
+        {/* </div> */}
         
-        <input type="radio" name="radio2" value="4" id="tab-2" />
-        <label for="tab-2" className= {styles.tabstwo} >
+        {/* <div > */}
+        <input type="radio" name="radio2" value="4" id="tab-2"/>
+        <label for="tab-2"className= {styles.insidetabs} >
             <p>Make your own </p></label>
+        {/* </div> */}
 
-        <input type="radio" name="radio2" value="5" id="tab-3" />
-        <label for="tab-3" className= {styles.tabsthree}>
+        {/* <div className= {styles.insidetabs}> */}
+        <input type="radio" name="radio2" value="5" id="tab-3"/>
+        <label for="tab-3" className= {styles.insidetabs}>
             <p>Share it</p></label>
+        {/* </div> */}
         
         {/* <div className={styles.tabsselected}></div> */}
       </div>
