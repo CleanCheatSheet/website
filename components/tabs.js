e1812dbdb7719c2e102292b4f7c06a4e220fc1a3
Lea Juskiewenski <lea.jski@gmail.com>
2021-10-01 12:15:08 +0200

    neumorphism design and display text

--- components/tabs.js
+++ components/tabs.js
@@ -1,30 +1,50 @@
 import styles from "../styles/Tabs.module.css";
+import React, { useState } from "react";
 // import Link from "next/link"
 // import NextImage from "next/image"
 
 export function Tabs(props) {
-    return (
-        <div className={styles.tabs}>
-      
-        {/* <div className= {styles.insidetabs}> */}
-        <input type="radio" name="radio2" value="3" id="tab-1" />
-        <label for="tab-1" className= {styles.insidetabs}>
-            <p>Search through the basics</p></label>
-        {/* </div> */}
-        
-        {/* <div > */}
-        <input type="radio" name="radio2" value="4" id="tab-2"/>
-        <label for="tab-2"className= {styles.insidetabs} >
-            <p>Make your own </p></label>
-        {/* </div> */}
+  
+  const [data, setData] = useState(props.content);
 
-        {/* <div className= {styles.insidetabs}> */}
-        <input type="radio" name="radio2" value="5" id="tab-3"/>
-        <label for="tab-3" className= {styles.insidetabs}>
-            <p>Share it</p></label>
-        {/* </div> */}
-        
-        {/* <div className={styles.tabsselected}></div> */}
-      </div>
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const [showCurrent, setShowCurrent] = useState(false);
+
+  const toggleCurrent = () => {
+    if (!showCurrent) {
+      setShowCurrent(true);
+      return;
+    }
+  };
+
+  const setCurrent = index => {
+    setCurrentIdx(index);
+    toggleCurrent();
+  };
+
+    return (
+        <div className={styles.grid}>
+          <div className={styles.tabs}>
+            <input type="radio" name="radio2" value="3" id="tab-1" onChange={() => setCurrent(0)} />
+            <label for="tab-1" className= {styles.insidetabs}>
+                <p>{props.title[0]}</p></label>
+                
+            <input type="radio" name="radio2" value="4" id="tab-2" onChange={() => setCurrent(1)}/>
+            <label for="tab-2"className= {styles.insidetabs} >
+                <p> {props.title[1]}</p></label>
+
+            <input type="radio" name="radio2" value="5" id="tab-3" onChange={() => setCurrent(2)}/>
+            <label for="tab-3" className= {styles.insidetabs}>
+                <p>{props.title[2]}</p></label>
+          </div>
+
+          <div className={styles.text}>
+            {showCurrent ? <div className="title">{data[currentIdx]['title']}</div> : null}
+            {showCurrent ? <p>{data[currentIdx]['paragraph']}</p> : null}
+          </div>
+          
+        </div>
+
+     
     );
   }
--- pages/about.js
+++ pages/about.js
@@ -3,16 +3,34 @@ import { Tabs } from '../components/tabs'
 
 
 export default function About() {
+  const title = [ "Search through the basics", "Make your own", "Share it"]
+  const content1 = []
+  content1['title'] = "Enjoy the power of collaboration."
+  content1['paragraph'] = "Search in the wide library of existing sheets to get all the tips and tricks of your everydays programming languages and tools."
+  
+  const content2 = []
+  content2['title'] = "Make your own personal cheat sheet."
+  content2['paragraph'] = "Use the Clean Cheat Sheet tool to format your very personal notes."
+  
+  const content3 = []
+  content3['title'] = "Contribute."
+  content3['paragraph'] = "Share your created sheets on github so that everybody can use it."
+  
+  const content = [ content1, content2, content3]
+  
+  console.log(content)
+
   return (
     <div>
       <Head>
         <title>about page</title>
       </Head>
-      <div className="title">
+      {/* <div className="title">
         Clean Cheat Sheet
-      </div>
+      </div> */}
 
-      <Tabs></Tabs>
+
+      <Tabs title={title} content={content}></Tabs>
 
         {/* <p>
       An open collaborative tool for creating designed and clean cheat sheets.
