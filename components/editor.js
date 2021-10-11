f3e607e6ff3ed60d0556870623279ec246c04c2e
airklizz <remi.calizzano@gmail.com>
2021-10-11 22:01:14 +0200

    create an editor component

---
+++ components/editor.js
@@ -0,0 +1,100 @@
+import { useEffect, useRef, useState } from "react";
+
+import { CleanCheatSheet } from "../components/sheet";
+import { createSheet } from "../pages/[path]";
+import styles from "../styles/Editor.module.css";
+
+function getSessionStorageOrDefault(key, defaultValue) {
+  const stored = sessionStorage.getItem(key);
+  if (!stored) {
+    return defaultValue;
+  }
+  return JSON.parse(stored);
+}
+
+export function Editor() {
+  const [input, setInput] = useState("");
+  const [data, setData] = useState({ color: "black", title: "title" });
+  const [sheets, setSheets] = useState([""]);
+  var isHandlerDragging = false;
+  const boxRef = useRef(null);
+  const handlerRef = useRef(null);
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    setInput(getSessionStorageOrDefault("input", ""));
+    document.addEventListener("mousedown", function (e) {
+      // If mousedown event is fired from .handler, toggle flag to true
+      console.log("Mousse down::", e.target);
+      if (e.target === handlerRef.current) {
+        console.log("On Handler");
+        isHandlerDragging = true;
+      }
+    });
+    document.addEventListener("mousemove", function (e) {
+      // Don't do anything if dragging flag is false
+      console.log("Mousse Drag::", isHandlerDragging);
+      if (!isHandlerDragging) {
+        return false;
+      }
+      // Get offset
+      var containerOffsetLeft = wrapperRef.current.offsetLeft;
+      // Get x-coordinate of pointer relative to container
+      var pointerRelativeXpos = e.clientX - containerOffsetLeft;
+      // Arbitrary minimum width set on box A, otherwise its inner content will collapse to width of 0
+      var boxAminWidth = 100;
+      // Resize box A
+      // * 8px is the left/right spacing between .handler and its inner pseudo-element
+      // * Set flex-grow to 0 to prevent it from growing
+      boxRef.current.style.width =
+        Math.max(boxAminWidth, pointerRelativeXpos - 8) + "px";
+      boxRef.current.style.flexGrow = 0;
+    });
+    document.addEventListener("mouseup", function (e) {
+      // Turn off dragging flag when user mouse is up
+      // setIsHandlerDragging(false);
+      isHandlerDragging = false;
+    });
+  }, []);
+
+  useEffect(() => {
+    sessionStorage.setItem("input", JSON.stringify(input));
+    updateSheet(input);
+  }, [input]);
+
+  async function updateSheet(input) {
+    const sheet = createSheet(
+      `${process.env.DOMAIN}/_next/image?w=256&q=100&url=`,
+      input
+    );
+    setData(sheet.data);
+    setSheets(sheet.sheets);
+  }
+
+  function handleOnChange(event) {
+    const input = event.target.value;
+    setInput(input);
+  }
+  return (
+    <div ref={wrapperRef} className={styles.wrapper}>
+      <div ref={boxRef} className={styles.container}>
+        <textarea
+          className={styles.textarea}
+          value={input}
+          autoFocus
+          onChange={handleOnChange}
+        ></textarea>
+      </div>
+      <div ref={handlerRef} className={styles.handler}></div>
+      <div className={styles.container}>
+        <div className={styles.sheet}>
+          <CleanCheatSheet
+            title={data.title}
+            color={data.firstColor}
+            sheets={sheets}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/create.js
+++ pages/create.js
@@ -1,48 +1,15 @@
 import { Action, App, Octokit } from "octokit";
 import { useEffect, useRef, useState } from "react";
 
-import { CleanCheatSheet } from "../components/sheet";
+import { Editor } from "../components/editor";
 import Head from "next/head";
 import { createPullRequest } from "octokit-plugin-create-pull-request";
-// import hljs from "highlight.js";
-// import marked from "marked";
-// import matter from "gray-matter";
-import { createSheet } from "./[path]";
 import styles from "../styles/Create.module.css";
 import { useSession } from "next-auth/react";
-
-// marked.setOptions({
-//   langPrefix: "hljs language-",
-//   highlight: function (code) {
-//     return hljs.highlightAuto(code, ["html", "javascript"]).value;
-//   },
-// });
-
-// export function createSheet(baseUrl, content) {
-//   const sheet = matter(content);
-//   marked.setOptions({ baseUrl: baseUrl });
-//   sheet.sheets = marked(sheet.content).split("+++");
-//   return sheet;
-// }
-
-function getSessionStorageOrDefault(key, defaultValue) {
-  const stored = sessionStorage.getItem(key);
-  if (!stored) {
-    return defaultValue;
-  }
-  return JSON.parse(stored);
-}
 
 export default function Create() {
   const MyOctokit = Octokit.plugin(createPullRequest);
 
-  const [input, setInput] = useState("");
-  const [data, setData] = useState({ color: "black", title: "title" });
-  const [sheets, setSheets] = useState([""]);
-  var isHandlerDragging = false;
-  const boxRef = useRef(null);
-  const handlerRef = useRef(null);
-  const wrapperRef = useRef(null);
   const [image, setImage] = useState(null);
   const [createObjectURL, setCreateObjectURL] = useState(null);
   const [uploadedFiles, setUploadedFiles] = useState([]);
@@ -144,61 +111,6 @@ export default function Create() {
     }
   }
 
-  useEffect(() => {
-    setInput(getSessionStorageOrDefault("input", ""));
-    document.addEventListener("mousedown", function (e) {
-      // If mousedown event is fired from .handler, toggle flag to true
-      console.log("Mousse down::", e.target);
-      if (e.target === handlerRef.current) {
-        console.log("On Handler");
-        isHandlerDragging = true;
-      }
-    });
-    document.addEventListener("mousemove", function (e) {
-      // Don't do anything if dragging flag is false
-      console.log("Mousse Drag::", isHandlerDragging);
-      if (!isHandlerDragging) {
-        return false;
-      }
-      // Get offset
-      var containerOffsetLeft = wrapperRef.current.offsetLeft;
-      // Get x-coordinate of pointer relative to container
-      var pointerRelativeXpos = e.clientX - containerOffsetLeft;
-      // Arbitrary minimum width set on box A, otherwise its inner content will collapse to width of 0
-      var boxAminWidth = 100;
-      // Resize box A
-      // * 8px is the left/right spacing between .handler and its inner pseudo-element
-      // * Set flex-grow to 0 to prevent it from growing
-      boxRef.current.style.width =
-        Math.max(boxAminWidth, pointerRelativeXpos - 8) + "px";
-      boxRef.current.style.flexGrow = 0;
-    });
-    document.addEventListener("mouseup", function (e) {
-      // Turn off dragging flag when user mouse is up
-      // setIsHandlerDragging(false);
-      isHandlerDragging = false;
-    });
-  }, []);
-
-  useEffect(() => {
-    sessionStorage.setItem("input", JSON.stringify(input));
-    updateSheet(input);
-  }, [input]);
-
-  async function updateSheet(input) {
-    const sheet = createSheet(
-      `${process.env.DOMAIN}/_next/image?w=256&q=100&url=`,
-      input
-    );
-    setData(sheet.data);
-    setSheets(sheet.sheets);
-  }
-
-  function handleOnChange(event) {
-    const input = event.target.value;
-    setInput(input);
-  }
-
   const formHandleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
@@ -278,30 +190,7 @@ export default function Create() {
           </div>
         </>
       )}
-      {!displayForm && (
-        <>
-          <div ref={wrapperRef} className={styles.wrapper}>
-            <div ref={boxRef} className={styles.container}>
-              <textarea
-                className={styles.textarea}
-                value={input}
-                autoFocus
-                onChange={handleOnChange}
-              ></textarea>
-            </div>
-            <div ref={handlerRef} className={styles.handler}></div>
-            <div className={styles.container}>
-              <div className={styles.sheet}>
-                <CleanCheatSheet
-                  title={data.title}
-                  color={data.firstColor}
-                  sheets={sheets}
-                />
-              </div>
-            </div>
-          </div>
-        </>
-      )}
+      {!displayForm && <Editor />}
     </>
   );
 }
