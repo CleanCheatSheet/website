139a981bdf922b3d380849ac5560e4f030bc7c09
airklizz <remi.calizzano@gmail.com>
2021-10-11 21:51:30 +0200

    form to create the merge request

--- pages/create.js
+++ pages/create.js
@@ -4,25 +4,26 @@ import { useEffect, useRef, useState } from "react";
 import { CleanCheatSheet } from "../components/sheet";
 import Head from "next/head";
 import { createPullRequest } from "octokit-plugin-create-pull-request";
-import hljs from "highlight.js";
-import marked from "marked";
-import matter from "gray-matter";
+// import hljs from "highlight.js";
+// import marked from "marked";
+// import matter from "gray-matter";
+import { createSheet } from "./[path]";
 import styles from "../styles/Create.module.css";
 import { useSession } from "next-auth/react";
 
-marked.setOptions({
-  langPrefix: "hljs language-",
-  highlight: function (code) {
-    return hljs.highlightAuto(code, ["html", "javascript"]).value;
-  },
-});
+// marked.setOptions({
+//   langPrefix: "hljs language-",
+//   highlight: function (code) {
+//     return hljs.highlightAuto(code, ["html", "javascript"]).value;
+//   },
+// });
 
-export function createSheet(baseUrl, content) {
-  const sheet = matter(content);
-  marked.setOptions({ baseUrl: baseUrl });
-  sheet.sheets = marked(sheet.content).split("+++");
-  return sheet;
-}
+// export function createSheet(baseUrl, content) {
+//   const sheet = matter(content);
+//   marked.setOptions({ baseUrl: baseUrl });
+//   sheet.sheets = marked(sheet.content).split("+++");
+//   return sheet;
+// }
 
 function getSessionStorageOrDefault(key, defaultValue) {
   const stored = sessionStorage.getItem(key);
@@ -45,6 +46,8 @@ export default function Create() {
   const [image, setImage] = useState(null);
   const [createObjectURL, setCreateObjectURL] = useState(null);
   const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [displayForm, setDisplayForm] = useState(false);
+  const [inputs, setInputs] = useState({});
 
   const uploadToClient = (event) => {
     if (event.target.files && event.target.files[0]) {
@@ -73,6 +76,7 @@ export default function Create() {
 
   function createRepo(name) {
     console.log("Create REPO function", session);
+    setDisplayForm(!displayForm);
     // if (typeof session !== "undefined" && session !== null) {
     //   const octokit = new MyOctokit({ auth: session.accessToken });
     //   console.log("OCTOKIT::", octokit);
@@ -100,33 +104,32 @@ export default function Create() {
         .createPullRequest({
           owner: "CleanCheatSheet",
           repo: "sheets",
-          title: "pull request test",
-          body: "pull request description",
+          title: "update color",
+          body: "change the color to lightgray to be able to read the text",
           base: "main" /* optional: defaults to default branch */,
-          head: "pull-request-branch-name",
+          head: "new-color",
           changes: [
             {
               /* optional: if `files` is not passed, an empty commit is created instead */
-              // files: {
-              //   "path/to/file1.txt": "Content for file1",
-              //   "path/to/file2.png": {
-              //     content: "_base64_encoded_content_",
-              //     encoding: "base64",
-              //   },
-              //   // deletes file if it exists,
-              //   "path/to/file3.txt": null,
-              //   // updates file based on current content
-              //   "path/to/file4.txt": ({ exists, encoding, content }) => {
-              //     // do not create the file if it does not exist
-              //     if (!exists) return null;
+              files: {
+                "git/README.md": input,
+                // "path/to/file2.png": {
+                //   content: "_base64_encoded_content_",
+                //   encoding: "base64",
+                // },
+                // // deletes file if it exists,
+                // "path/to/file3.txt": null,
+                // // updates file based on current content
+                // "path/to/file4.txt": ({ exists, encoding, content }) => {
+                //   // do not create the file if it does not exist
+                //   if (!exists) return null;
 
-              //     return Buffer.from(content, encoding)
-              //       .toString("utf-8")
-              //       .toUpperCase();
-              //   },
-              // },
-              commit:
-                "creating file1.txt, file2.png, deleting file3.txt, updating file4.txt (if it exists)",
+                //   return Buffer.from(content, encoding)
+                //     .toString("utf-8")
+                //     .toUpperCase();
+                // },
+              },
+              commit: "change color",
             },
           ],
         })
@@ -196,6 +199,17 @@ export default function Create() {
     setInput(input);
   }
 
+  const formHandleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const formHandleSubmit = (event) => {
+    event.preventDefault();
+    console.log("INPUTSS", inputs);
+  };
+
   return (
     <>
       <Head>
@@ -219,26 +233,75 @@ export default function Create() {
         </button>
         {JSON.stringify(uploadedFiles)}
       </div>
-      <div ref={wrapperRef} className={styles.wrapper}>
-        <div ref={boxRef} className={styles.container}>
-          <textarea
-            className={styles.textarea}
-            value={input}
-            autoFocus
-            onChange={handleOnChange}
-          ></textarea>
-        </div>
-        <div ref={handlerRef} className={styles.handler}></div>
-        <div className={styles.container}>
-          <div className={styles.sheet}>
-            <CleanCheatSheet
-              title={data.title}
-              color={data.firstColor}
-              sheets={sheets}
-            />
+      {displayForm && (
+        <>
+          <div className={styles.form}>
+            <form onSubmit={formHandleSubmit}>
+              <label>
+                Title:
+                <input
+                  type="text"
+                  name="title"
+                  value={inputs.title || ""}
+                  onChange={formHandleChange}
+                />
+              </label>
+              <label>
+                Body:
+                <input
+                  type="text"
+                  name="body"
+                  value={inputs.body || ""}
+                  onChange={formHandleChange}
+                />
+              </label>
+              <label>
+                Head:
+                <input
+                  type="text"
+                  name="head"
+                  value={inputs.head || ""}
+                  onChange={formHandleChange}
+                />
+              </label>
+              <label>
+                Commit:
+                <input
+                  type="text"
+                  name="commit"
+                  value={inputs.commit || ""}
+                  onChange={formHandleChange}
+                />
+              </label>
+              <input type="submit" />
+            </form>
           </div>
-        </div>
-      </div>
+        </>
+      )}
+      {!displayForm && (
+        <>
+          <div ref={wrapperRef} className={styles.wrapper}>
+            <div ref={boxRef} className={styles.container}>
+              <textarea
+                className={styles.textarea}
+                value={input}
+                autoFocus
+                onChange={handleOnChange}
+              ></textarea>
+            </div>
+            <div ref={handlerRef} className={styles.handler}></div>
+            <div className={styles.container}>
+              <div className={styles.sheet}>
+                <CleanCheatSheet
+                  title={data.title}
+                  color={data.firstColor}
+                  sheets={sheets}
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
